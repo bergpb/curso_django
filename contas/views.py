c94c4f8fac21f1e550bcd82c56f2561ca729d986
bergpb <lindembergdepaulo@gmail.com>
2019-07-11 23:45:35 -0300

    first commit

---
+++ contas/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+import datetime
+
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+    data['now'] = datetime.datetime.now()
+    return render(request, 'contas/home.html', data)
